89e46029afbad8f51a4ee7147f29f6cec4507024
Snakultime <snakultime@hotmail.fr>
2018-10-25 18:03:10 +0200

    ajout v1

--- db/migrate/20181025132846_create_dogsitters.rb
+++ db/migrate/20181025132846_create_dogsitters.rb
@@ -1,7 +1,7 @@
 class CreateDogsitters < ActiveRecord::Migration[5.2]
   def change
     create_table :dogsitters do |t|
-
+      t.string :name
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+    def dog
+        15.times do
+        Dog.create!(name: Faker::Kpop.solo, city_id: Faker::Number.between(City.first.id, City.last.id))
+        end
+      end
+
+    def dogsitter
+        10.times do
+        Dogsitter.create!(name: Faker::Name.name, city_id: Faker::Number.between(City.first.id, City.last.id))
+        end
+    end
+
+    def city
+        5.times do
+        City.create!(city_name: Faker::Address.street_name)
+        end
+    end
+
+    def stroll
+      15.times do
+        Stroll.create!(dog_id: Faker::Number.between(Dog.first.id, Dog.last.id), dogsitter_id: Faker::Number.between(Dogsitter.first.id, Dogsitter.last.id))
+      end
+    end
+city
+dog
+dogsitter
+stroll
